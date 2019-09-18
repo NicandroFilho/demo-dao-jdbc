a88dabb50e6c431f74c00430f8af6f74816a61e5
NicandroFilho <nicandro.m@gmail.com>
2019-09-18 19:20:37 -0300

    Department findAll implementation

--- src/application/TestesDepartmentDao.java
+++ src/application/TestesDepartmentDao.java
@@ -5,6 +5,7 @@ import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class TestesDepartmentDao {
@@ -15,7 +16,13 @@ public class TestesDepartmentDao {
         System.out.println("TESTE 1: Department Find by Id");
         Department dep = departmentDao.findById(1);
         System.out.println(dep);
+        System.out.println();
 
+        System.out.println("TESTE 2: Department Find All");
+        List<Department> list = departmentDao.findAll();
+        for (Department d : list){
+            System.out.println(d);
+        }
 
         DB.closeConnection();
         sc.close();
