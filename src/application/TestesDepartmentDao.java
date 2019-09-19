40e10671b5995ac5a2a0ab7cde25f67acf71eb00
NicandroFilho <nicandro.m@gmail.com>
2019-09-19 12:15:37 -0300

    Department Updates in TestDepartmentDao

--- src/application/TestesDepartmentDao.java
+++ src/application/TestesDepartmentDao.java
@@ -14,7 +14,10 @@ public class TestesDepartmentDao {
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
         System.out.println("TEST 1: Department Find by Id");
-        Department dep = departmentDao.findById(1);
+        System.out.print("Enter the Department Id to be searched: ");
+        int findId = sc.nextInt();
+        sc.nextLine();
+        Department dep = departmentDao.findById(findId);
         System.out.println(dep);
         System.out.println();
 
@@ -26,25 +29,32 @@ public class TestesDepartmentDao {
         System.out.println();
 
         System.out.println("TEST 3: Department Insert");
-        dep = new Department(null, "Food");
-        departmentDao.insert(dep);
-        System.out.println("New Department Created! Department Id: " + dep.getId());
+        System.out.print("Enter Department name to be Inserted into Data Base: ");
+        String newDepName = sc.nextLine();
+        Department newDep = new Department(null, newDepName);
+        departmentDao.insert(newDep);
+        System.out.println("New Department Created! Department Id: " + newDep.getId());
         System.out.println();
 
         System.out.println("TEST 4: Department Delete");
         System.out.print("Enter the Id to be Deleted: ");
-        int id = sc.nextInt();
-        departmentDao.deleteById(id);
+        int delId = sc.nextInt();
+        departmentDao.deleteById(delId);
         System.out.println();
 
         System.out.println("TEST 5: Department Update");
-        System.out.println("Updating Department "+ dep.getName());
+        System.out.print("Enter the Department Number which will be updated: ");
+        int updateDepNum = sc.nextInt();
+        sc.nextLine();
+        Department depForUpdate = departmentDao.findById(updateDepNum);
+        System.out.println("Updating Department "+ depForUpdate.getName());
         System.out.print("Enter new Department Name: ");
         String newName = sc.nextLine();
-        dep.setName(newName);
-        departmentDao.update(dep);
+        depForUpdate.setName(newName);
+        System.out.println("Updating DataBase");
+        departmentDao.update(depForUpdate);
         System.out.println("Retrieving new Data from DataBase:");
-        System.out.println(departmentDao.findById(dep.getId()));
+        System.out.println(departmentDao.findById(depForUpdate.getId()));
         System.out.println();
 
         DB.closeConnection();
